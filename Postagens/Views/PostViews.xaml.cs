b606e29f8bee59c027043e414433968ec11795ea
rafaela-oliveira21 <rafalelaoliveira20@email.com>
2025-06-06 22:39:49 -0300

    PAMI-POST

---
+++ Postagens/Views/PostViews.xaml.cs
@@ -0,0 +1,14 @@
+namespace Postagens.Views;
+
+public partial class PostViews : ContentPage
+{
+	public parcial class PostViews : ContegePage
+	{
+
+		public PostViews()
+		{
+		BindingContext = new PostViews();
+		InitializeComponent();
+		}
+	}
+}
